75c3bcf95ba88d249e2e0fd67d355384d98867b5
Luis Henrique Tessaro Pedroso <luis.pedroso@lcs.local>
2019-04-30 15:07:05 -0300

    404 route

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use(express.static('./public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(require('./controllers'))
+app.get('*', function (req, res) {
+    res.send('no route implemented there yet', 404)
+})
 
 
 app.listen(port, () => {
