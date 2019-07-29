f6975a0baa45c5d799e59266abe411e0cc036a56
LuisTessaro <lh.pedrosot@hotmail.com>
2019-07-29 10:30:50 -0300

    jwt middleware

---
+++ middlewares/jwt.js
@@ -0,0 +1,8 @@
+const checkTokenMiddleware = (req, res, next) => {
+    const accessToken = req.headers['x-access-token']
+    if (!accessToken) return res.status(400).send('Please provide a x-access-token')
+    jwt.verify(accessToken, process.env.SECRET, (err, decoded) => {
+        if (err) return res.status(400).send('Please provide a valid token')
+        next()
+    })
+}
