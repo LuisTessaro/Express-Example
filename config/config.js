950c0246cc08c80f43e68c78ea908afb34e9da5e
mablds <marcelobragalemos@gmail.com>
2020-01-24 11:20:03 -0300

    body-parsed removed

--- config/config.js
+++ config/config.js
@@ -2,13 +2,12 @@ const express = require('express')
 
 module.exports.setUpServer = () => {
     const app = express()
-    const bodyParser = require('body-parser')
 
     app.set('view engine', 'pug')
 
     app.use(express.static('../public'))
 
-    app.use(bodyParser.json())
+    app.use(express.json())
     app.use(bodyParser.urlencoded({ extended: true }))
 
     app.use(require('../controllers'))
